604d59b5a289925aa6b5b1082b781c8a963ecaf9
PPIR <ppirc@lemo.com>
2025-08-07 07:54:20 +0200

    Implemented

--- hello.py
+++ hello.py
@@ -1,5 +1,19 @@
+import pyautogui
+import random
+import time
+
 def main():
-    print("Hello from mm!")
+    screen_width, screen_height = pyautogui.size()
+    try:
+        while True:
+            
+            x = random.randint(0, screen_width-1)
+            y = random.randint(0, screen_height-1)
+            pyautogui.moveTo(x, y, duration=0.5)
+        
+            time.sleep(random.randint(1, 5))
+    except KeyboardInterrupt:
+        print("Stopped by user")
 
 
 if __name__ == "__main__":
